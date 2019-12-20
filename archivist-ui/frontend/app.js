93507e10253f42d935b8cecd6659d8aeda68074c
Szymon Kaliski <hi@szymonkaliski.com>
2019-12-20 15:56:55 +0100

    grid view

--- archivist-ui/frontend/app.js
+++ archivist-ui/frontend/app.js
@@ -1,6 +1,57 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
+const { Grid, AutoSizer } = require("react-virtualized");
 
-const App = () => <div>hi!</div>;
+const DATA = [];
+
+for (let i = 0; i < 10000; i++) {
+  DATA.push(`item: ${i}`);
+}
+
+const createCellRenderer = ({ columnCount }) => ({
+  columnIndex,
+  rowIndex,
+  key,
+  style
+}) => {
+  const index = columnIndex + rowIndex * columnCount;
+  const hasData = !!DATA[index];
+
+  return (
+    <div key={key} style={style}>
+      {hasData ? (
+        <div className="ba b--light-gray h-100">{DATA[index]}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const calcCellSize = ({ width }) => {
+  return width / Math.floor(width / 300);
+};
+
+const App = () => (
+  <div className="sans-serif w-100 vh-100">
+    <AutoSizer>
+      {({ width, height }) => {
+        const cellSize = calcCellSize({ width, height });
+        const columnCount = Math.max(width / cellSize, 1);
+        const cellRenderer = createCellRenderer({ columnCount });
+
+        return (
+          <Grid
+            cellRenderer={cellRenderer}
+            columnCount={columnCount}
+            columnWidth={cellSize}
+            rowHeight={cellSize}
+            rowCount={Math.ceil(DATA.length / columnCount)}
+            height={height}
+            width={width}
+          />
+        );
+      }}
+    </AutoSizer>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
